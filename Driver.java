b01ed910ca7682513e7601c73d0299805cfdc932
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-02-07 14:37:07 +0100

    Driver efter redovisningen

--- Driver.java
+++ Driver.java
@@ -6,36 +6,46 @@ import lab2.level.Level;
 import lab2.level.LevelGUI;
 import lab2.level.Room;
 
+
 public class Driver {
 
 	public void run() {
+	
+		Level lvl = new Level();//Skapar ett nytt level objekt.
+        
+		//Skapar 5 nya room objekt. 
+        	Room r1 = new Room(100, 100, Color.green);
+        	Room r2 = new Room(100,100,Color.orange);
+        	Room r3 = new Room(100, 100, Color.red);
+        	Room r4 = new Room(100,100,Color.PINK);
+        	Room r5 = new Room(100, 100, Color.CYAN);
+        	Room r6 = new Room(100,100, Color.blue);
+       
+        	//Tilldelar rummen connections.
+        	r1.connectEastTo(r2);
+        	r2.connectWestTo(r1);
+        	r2.connectNorthTo(r3);
+        	r3.connectSouthTo(r2);
+        	r4.connectEastTo(r3);
+        	r3.connectWestTo(r3);
+        	//r5.connectWestTo(r3);
+        	r3.connectEastTo(r5);
+        	r5.connectEastTo(r4);
+        	r4.connectEastTo(r1);
+        	r1.connectSouthTo(r6);
+       
 
-		Level lvl = new Level();
-		
-		Room r1 = new Room(10, 10, Color.blue);
-		Room r2 = new Room(10,10,Color.orange);
-		Room r3 = new Room(10, 10, Color.red);
-		Room r4 = new Room(10,10,Color.green);
-		Room r5 = new Room(10, 10, Color.yellow);
-		
-
-		r1.connectNorthTo(r2);
-		r2.connectSouthTo(r1);
-		r2.connectNorthTo(r3);
-		r3.connectSouthTo(r2);
-		r4.connectEastTo(r3);
-		r3.connectWestTo(r3);
-		r5.connectWestTo(r3);
-		r3.connectEastTo(r5);
-
-
-		System.out.println(lvl.place(r1, 11, 11));
-		System.out.println(lvl.place(r2, 11, 21));
-		System.out.println(lvl.place(r3, 11, 31));
-		System.out.println(lvl.place(r4, 1, 31));
-		System.out.println(lvl.place(r5, 21, 31));
-
-
+        	//Placerar ut rummen på leveln.
+        	System.out.println(lvl.place(r1, 0, 0));
+        	System.out.println(lvl.place(r2, 100, 0));
+        	System.out.println(lvl.place(r3, 400, 500));
+        	System.out.println(lvl.place(r4, 500, 620));
+        	System.out.println(lvl.place(r5, 600, 200));
+        	System.out.println(lvl.place(r6, 10, 10));
+        
+        	//Tilldelar startposition.
+        	lvl.firstLocation(r1);
+        
+        	new LevelGUI(lvl,"level");  
 	}
-
 }
