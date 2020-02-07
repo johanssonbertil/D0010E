be5672e3d2ef1a4b6ce6d3bd2af0047bbeec7ef6
Emn98 <60701147+Emn98@users.noreply.github.com>
2020-02-07 16:50:11 +0100

    Level som redovisades

--- Level.java
+++ Level.java
@@ -1,15 +1,20 @@
 package lab2.level;
 
-import java.util.Vector;
+import java.util.ArrayList;
 import java.util.Observable;
+import java.util.Vector;
 
 
-public class Level extends Observable {
+public class Level extends Observable{
 	
-    Vector<Room> rooms = new Vector<Room>();
+	Vector<Room> rooms = new Vector<Room>();
     Boolean firstLocationAdded = false;
     Room currentRoom = null;
 	
+    /*Denna metod kontrollerar samt sparar de godkända rum som man vill lägga till leveln. 
+     * Detta görs genom att kolla så att något rum inte överlappar med något annat rum. Om ingen
+     * överlappning sker kommer de godkända rummen att sparas till en vektor.
+     */
 	public boolean place(Room r, int x, int y){
 	
 		if(firstLocationAdded){ return false; }
@@ -21,16 +26,16 @@ public class Level extends Observable {
         for (int i = 0; i < rooms.size(); i++){
             Room room = rooms.get(i);
             if (room.x() >= r.x()){
-                if (r.x() + r.xSize()-1 >= room.x()){ xOverlap = true; }
+                if (r.x() + r.xSize() > room.x()){ xOverlap = true; }
             }
-            if (room.x() < r.x()){
-                if (room.x() + room.xSize()-1 >= r.x()){ xOverlap = true; }
+            else{
+                if (room.x() + room.xSize() > r.x()){ xOverlap = true; }
             }
-            if (room.y() <= r.y()){
-                if ( r.y() - r.ySize() < room.y()){ yOverlap = true; }
+            if (room.y() >= r.y()){
+                if ( r.y() + r.ySize() > room.y()){ yOverlap = true; }
             }
-            if (room.y() > r.y()){
-                if (room.y() - room.ySize() < r.y()){ yOverlap = true;}
+            else{
+                if (room.y() + room.ySize() > r.y()){ yOverlap = true;}
             }
         }
         
@@ -42,15 +47,22 @@ public class Level extends Observable {
 
 	}
 	
+	//Denna metod tilldelar det rum som spelaren kommer att börja i. 
 	public void firstLocation(Room r) {
 		firstLocationAdded = true;
 	    currentRoom = r;
 	}
 	
-	public Room getFirstLocation() {
+	//Denna metod returnerar det rum som spelaren i nuläget befinner sig i. 
+	public Room getCurrentRoom() {
 		return currentRoom;
 	}
 
+	/*
+	 * Denna metod kontrollerar om det rum som spelaren befinner sig i
+	 * har någon förbindelse med ett annat rum åt norr. Om detta stämmer
+	 * går spelaren till det rummet. 
+	 */
 	void moveNorth() {
 		if(currentRoom.northRoom != null) {
 			currentRoom = currentRoom.northRoom;
@@ -58,6 +70,11 @@ public class Level extends Observable {
 		}
 	}
 	
+	/*
+	 * Denna metod kontrollerar om det rum som spelaren befinner sig i
+	 * har någon förbindelse med ett annat rum åt söder. Om detta stämmer
+	 * går spelaren till det rummet. 
+	 */
 	void moveSouth() {
 		if(currentRoom.southRoom != null) {
 			currentRoom = currentRoom.southRoom;
@@ -65,6 +82,11 @@ public class Level extends Observable {
 		}
 	}
 	
+	/*
+	 * Denna metod kontrollerar om det rum som spelaren befinner sig i
+	 * har någon förbindelse med ett annat rum åt väst. Om detta stämmer
+	 * går spelaren till det rummet. 
+	 */
 	void moveWest() {
 		if(currentRoom.westRoom != null) {
 			currentRoom = currentRoom.westRoom;
@@ -72,6 +94,11 @@ public class Level extends Observable {
 		}
 	}
 	
+	/*
+	 * Denna metod kontrollerar om det rum som spelaren befinner sig i
+	 * har någon förbindelse med ett annat rum åt öst. Om detta stämmer
+	 * går spelaren till det rummet. 
+	 */
 	void moveEast() {
 		if(currentRoom.eastRoom != null) {
 			currentRoom = currentRoom.eastRoom;
@@ -79,6 +106,7 @@ public class Level extends Observable {
 		}
 	}
 	
+	//Denna metod notifierar alla observerare att förändringar har skett. 
 	void updateLevel() {
 		setChanged();
 		notifyObservers();
